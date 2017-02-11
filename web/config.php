f7c6a1873c90ec8d854fc37f8cc88f5f79acd604
Mátyás Somfai <matyas.somfai@cheppers.com>
2017-02-11 17:55:16 +0100

    symfony 2.6 -> 2.7 upgrade

--- web/config.php
+++ web/config.php
@@ -4,7 +4,7 @@ if (!isset($_SERVER['HTTP_HOST'])) {
     exit('This script cannot be run from the CLI. Run it from a browser.');
 }
 
-if (!in_array(@$_SERVER['REMOTE_ADDR'], array(
+if (getenv('DOCKER') !== '1' && !in_array(@$_SERVER['REMOTE_ADDR'], array(
     '127.0.0.1',
     '::1',
 ))) {
